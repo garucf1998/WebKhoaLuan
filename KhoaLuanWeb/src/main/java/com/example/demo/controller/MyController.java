900c50db103a1c5879f0c799d4d5d0f16e179e07
duyvien <duyvien159@gmail.com>
2021-05-28 10:49:15 +0700

    123

--- KhoaLuanWeb/src/main/java/com/example/demo/controller/MyController.java
+++ KhoaLuanWeb/src/main/java/com/example/demo/controller/MyController.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.security.Principal;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -19,12 +20,14 @@ import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -41,9 +44,6 @@ import enity.LichHen;
 import enity.NhanVien;
 import enity.TaiKhoan;
 
-
-
-
 @Controller
 public class MyController {
 	@Autowired
@@ -52,158 +52,203 @@ public class MyController {
 	BenhNhanService benhnhanservice;
 	@Autowired
 	NhanVienService nhanvienservice;
-	@Autowired 
+	@Autowired
 	LichHenService lichhenservice;
 	@Autowired
-    public JavaMailSender javaMailSender;
-	 
-	
-	@GetMapping(value = {"/","/trang-chu"})
-	public String hienThiTrangChu() {
+	public JavaMailSender javaMailSender;
+
+	@GetMapping(value = { "/", "/trang-chu" })
+	public String hienThiTrangChu(Principal principal, Model model) {
+		if (principal != null) {
+			System.out.println(principal.getName());
+		}
+		Object principal1 = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		// BCryptPasswordEncoder passwordEncoder =new BCryptPasswordEncoder();
+		// System.out.println("mat khau la :" +passwordEncoder.encode("anh"));
+		
+		if (principal != null) {
+			String username = principal.getName();
+			model.addAttribute("us", username);
+			try {
+				model.addAttribute("chucvu",
+						benhnhanservice.GetOneBenhNhanByUser(username).getTaiKhoan().getRole().getName());
+				model.addAttribute("name",
+						benhnhanservice.GetOneBenhNhanByUser(username).getTen());
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			// System.out.println("username là :" + username);
+			try {
+				System.out.println("Tên Chức Danh :"
+						+ benhnhanservice.GetOneBenhNhanByUser(username).getTaiKhoan().getRole().getName());
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}else {
+			model.addAttribute("chucvu", null);
+		}
 		return "index";
 	}
-	
+
 	@GetMapping("/dich-vu")
 	public String hienThiDanhSachDichVu() {
 		return "dich-vu";
 	}
+
 	@GetMapping("/doimatkhau")
 	public String hienthidoiMatKhau() {
-		
-			return "doi-mat-khau";
-		
+
+		return "doi-mat-khau";
+
 	}
+
 	@PostMapping("/doimatkhau")
-	public String doiMatKhau(@RequestParam("matkhaucu")String matkhaucu,
-			@RequestParam("matkhaumoi")String matkhaumoi,
-			@RequestParam("nhaplaimatkhau")String nhaplaimatkhau,Principal principal, RedirectAttributes redirectAttributes
-			) throws IOException{
-		
-		TaiKhoan tk= new TaiKhoan();
-		tk=taikhoanservice.GetOneTaiKhoan(principal.getName());
-		if(tk.getPassword().equals(matkhaucu))
-		{
-			if(matkhaumoi.equals(nhaplaimatkhau))
-			{
+	public String doiMatKhau(@RequestParam("matkhaucu") String matkhaucu, @RequestParam("matkhaumoi") String matkhaumoi,
+			@RequestParam("nhaplaimatkhau") String nhaplaimatkhau, Principal principal,
+			RedirectAttributes redirectAttributes) throws IOException {
+
+		TaiKhoan tk = new TaiKhoan();
+		tk = taikhoanservice.GetOneTaiKhoan(principal.getName());
+		if (tk.getPassword().equals(matkhaucu)) {
+			if (matkhaumoi.equals(nhaplaimatkhau)) {
 				tk.setPassword(matkhaumoi);
 				taikhoanservice.UpdateTK(tk);
-			}
-			else
-			{
-				redirectAttributes.addFlashAttribute("thatbai", "Đổi mật khẩu thất bại!- Mật khẩu mới không giống với xác nhận mật khẩu");
+			} else {
+				redirectAttributes.addFlashAttribute("thatbai",
+						"Đổi mật khẩu thất bại!- Mật khẩu mới không giống với xác nhận mật khẩu");
 				return "redirect:/doi-mat-khau";
 			}
-		}
-		else
-		{
+		} else {
 			redirectAttributes.addFlashAttribute("thatbai", "Đổi mật khẩu thất bại!- Bạn đã điền sai mật khẩu cũ");
 			return "redirect:/doi-mat-khau";
 		}
 		redirectAttributes.addFlashAttribute("thanhcong", "Đổi mật khẩu thành công!-");
 		return "/logout";
 	}
-	
+
 	@GetMapping(value = "/dang-nhap")
 	public String showSignIn(HttpSession session, Model model) {
-		
-		
-			TaiKhoan taiKhoan=new TaiKhoan();
-			model.addAttribute("taiKhoan", new TaiKhoan());
-			if(session.getAttribute("username")!=null) {
-				session.invalidate();
-			}
-			return "dang-nhap";
-	
+
+		TaiKhoan taiKhoan = new TaiKhoan();
+		model.addAttribute("taiKhoan", new TaiKhoan());
+		if (session.getAttribute("username") != null) {
+			session.invalidate();
+		}
+		return "dang-nhap";
+
 	}
+
 	@GetMapping("/thong-tin")
-	public String thongTinCaNhan() {
+	public String thongTinCaNhan(Principal principal, Model model) {
+		Object principal1 = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+		BenhNhan bn = new BenhNhan();
+		try {
+			bn = benhnhanservice.GetOneBenhNhanByUser(principal.getName());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+		String stringDate=df.format(bn.getNgaySinh());
+		model.addAttribute("ngaysinh", stringDate);
+		model.addAttribute("benhnhan", bn);
+		List<LichHen> lichHen = new ArrayList<LichHen>();
+		try {
+			lichHen = lichhenservice.GetAllLichHenByBenhNhan(bn.getId());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println("Tên :"+bn.getTen());
+		model.addAttribute("lichHen", lichHen);
+
 		return "thong-tin";
+
 	}
-	
+
 	@GetMapping("/dang-ky")
 	public String hienThiDangKy(Model model) {
-		BenhNhan benhNhan=new BenhNhan();
+		BenhNhan benhNhan = new BenhNhan();
 		benhNhan.setTaiKhoan(new TaiKhoan());
 		model.addAttribute("benhNhan", new BenhNhan());
 		return "dang-ky";
 	}
+
 	@PostMapping("/dang-ky")
-	public String dangKy(@ModelAttribute("benhNhan")BenhNhan benhNhan,@ModelAttribute("taikhoan")TaiKhoan taikhoan,@RequestParam("ngay_sinh")String ngaysinh,RedirectAttributes redirectAttributes) throws IOException {
-		
-		TaiKhoan tkExist=null;
-		tkExist=taikhoanservice.GetOneTaiKhoan(taikhoan.getUsername());
-		if(tkExist.getUsername()!=null) {
+	public String dangKy(@ModelAttribute("benhNhan") BenhNhan benhNhan, @ModelAttribute("taikhoan") TaiKhoan taikhoan,
+			@RequestParam("ngay_sinh") String ngaysinh, RedirectAttributes redirectAttributes) throws IOException {
+
+		TaiKhoan tkExist = null;
+		tkExist = taikhoanservice.GetOneTaiKhoan(taikhoan.getUsername());
+		if (tkExist.getUsername() != null) {
 			redirectAttributes.addFlashAttribute("thatbai", "Đăng ký thất bại!- Trùng username");
 			return "index";
 		}
-		
+
 		taikhoan.setRole(benhnhanservice.GetOneRole((long) 5));
 		taikhoan.setPassword("123456");
-		int ketquaAddTK=taikhoanservice.POSTRequest(taikhoan);
-		if(ketquaAddTK==200)
-		{
+		int ketquaAddTK = taikhoanservice.POSTRequest(taikhoan);
+		if (ketquaAddTK == 200) {
 			benhNhan.setTaiKhoan(taikhoan);
 			SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
-			Date date=new Date();
-	        try {
-	             date = formatter.parse(ngaysinh);
-	        } catch (ParseException e) {
-	            e.printStackTrace();
-	        }
+			Date date = new Date();
+			try {
+				date = formatter.parse(ngaysinh);
+			} catch (ParseException e) {
+				e.printStackTrace();
+			}
 			benhNhan.setNgaySinh(date);
-			int ketquaPOST=benhnhanservice.POSTBenhNhan(benhNhan);
-			if (ketquaPOST==200)
-			{
-				BCryptPasswordEncoder encoder=new BCryptPasswordEncoder();
+			int ketquaPOST = benhnhanservice.POSTBenhNhan(benhNhan);
+			if (ketquaPOST == 200) {
+				BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 				taikhoan.setPassword(encoder.encode(taikhoan.getPassword()));
 				redirectAttributes.addFlashAttribute("thanhcong", "Đăng ký thành công!");
 				return "redirect:/dang-nhap";
-			}
-			else
-			{
+			} else {
 				redirectAttributes.addFlashAttribute("thatbai", "Đăng ký thất bại!");
-				int ketqua=taikhoanservice.DeleteTaiKhoan(taikhoan.getUsername());
+				int ketqua = taikhoanservice.DeleteTaiKhoan(taikhoan.getUsername());
 				return "redirect:/dang-ky";
-				
+
 			}
 		}
 		redirectAttributes.addFlashAttribute("thatbai", "Đăng ký thất bại!");
 		return "redirect:/dang-ky";
 	}
 
-	
-	
 	@PostMapping("/doLogin")
-	public String dangNhap(@ModelAttribute("taiKhoan")TaiKhoan taikhoan,RedirectAttributes redirectAttributes) throws IOException {
-		
+	public String dangNhap(@ModelAttribute("taiKhoan") TaiKhoan taikhoan, RedirectAttributes redirectAttributes)
+			throws IOException {
+
 		System.out.println("User name :" + taikhoan.getUsername());
-		TaiKhoan tkExist=taikhoanservice.GetOneTaiKhoan(taikhoan.getUsername());
-		if(taikhoan.getUsername()!=null)	
-		{
-			if(tkExist.getPassword().equals(taikhoan.getPassword()))
+		TaiKhoan tkExist = taikhoanservice.GetOneTaiKhoan(taikhoan.getUsername());
+		if (taikhoan.getUsername() != null) {
+			if (tkExist.getPassword().equals(taikhoan.getPassword()))
 				return "redirect:/index";
 		}
-		
+
 		return "redirect:/dang-nhap";
 	}
-	
+
 	@GetMapping("/thong-bao")
 	public String thongBaoThanhCong() {
 		return "thong-bao";
 	}
-	
+
 	@GetMapping("/logout")
 	public String logoutPage(HttpServletRequest request, HttpServletResponse response) {
-        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        if (auth != null) {
-            new SecurityContextLogoutHandler().logout(request, response, auth);
-        }
-        return "redirect:/dang-nhap";
-    }
-	
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		if (auth != null) {
+			new SecurityContextLogoutHandler().logout(request, response, auth);
+		}
+		return "redirect:/dang-nhap";
+	}
+
 	@GetMapping("/dat-lich")
 	public String hienThiDatLich(Model model, Principal principal) throws IOException {
-		
+
 		TaiKhoan user = taikhoanservice.GetOneTaiKhoan(principal.getName());
 		if (user.getUsername() == null) {
 			return "redirect:/dang-nhap";
@@ -216,9 +261,10 @@ public class MyController {
 		System.out.println("User name :" + nhanVien.getTen());
 		return "dat-lich";
 	}
+
 	@PostMapping("/dat-lich")
 	public String datLich(@ModelAttribute("lichHen") LichHen lichHen,
-			@RequestParam(value = "manhanvien", required = false) String manhanvien, Principal principal, 
+			@RequestParam(value = "manhanvien", required = false) String manhanvien, Principal principal,
 			@RequestParam(value = "thoigiankham", required = false) String thoigiankham,
 			RedirectAttributes redirectAttributes) throws IOException {
 		TaiKhoan user = taikhoanservice.GetOneTaiKhoan(principal.getName());
@@ -226,58 +272,56 @@ public class MyController {
 			return "redirect:/dang-nhap";
 		}
 		BenhNhan benhNhan = benhnhanservice.GetOneBenhNhanByUser(principal.getName());
-		
-		NhanVien nhanVien = nhanvienservice.GetOneNhanVien((long)(Integer.parseInt(manhanvien)));
+
+		NhanVien nhanVien = nhanvienservice.GetOneNhanVien((long) (Integer.parseInt(manhanvien)));
 		System.out.println("User name :" + nhanVien.getTen());
 		SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
-		Date date=new Date();
-        try {
-             date = formatter.parse(thoigiankham);
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-        LichHen lh=null;
-        lh=lichhenservice.GetLichHenBenhNhan(lichhenservice.doichuoitungay(date), benhNhan.getId());	
-		lichHen.setBenhNhan(benhNhan);
+		Date date = new Date();
+		try {
+			date = formatter.parse(thoigiankham);
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		LichHen lh = null;
+		lh = lichhenservice.GetLichHenBenhNhan(lichhenservice.doichuoitungay(date), benhNhan.getId());
+		lichHen.setBenhnhan(benhNhan);
 		lichHen.setNhanVien(nhanVien);
 		lichHen.setThoiGian(date);
 		lichHen.setHinhThuc(true);
 		lichHen.setTrangThai("3");
-		if(lh==null)
-		{
-			int ketqua=lichhenservice.POSTLichHen(lichHen);
-			
-			if(ketqua==200)
-			{
-				
+		if (lh == null) {
+			int ketqua = lichhenservice.POSTLichHen(lichHen);
+
+			if (ketqua == 200) {
+
 				Thread thread = new Thread(() -> {
 					MimeMessage message = javaMailSender.createMimeMessage();
 					MimeMessageHelper messageHelper = new MimeMessageHelper(message);
 					try {
-						
-					messageHelper.setSubject("Xác nhận đặt lịch tại phòng khám");
-					messageHelper.setFrom("phongkhamlv@gmail.com", "Phòng khám LV");
-					messageHelper.setTo(benhNhan.getEmail());
-					String content = "<b>Xin chào " + benhNhan.getTen() + "</b> <br>";
-					content += "Chúng tôi đã nhận được lịch hẹn trước của bạn tại phòng khám với các thông tin như sau : <br>";
-					content += "Họ và tên :" + benhNhan.getTen() + "<br>";
-					content += "Ngày sinh : " + benhnhanservice.doichuoitungay(benhNhan.getNgaySinh()) + "<br>";
-					String gioitinh="";
-					if(benhNhan.isGioiTinh())
-						gioitinh="Nam";
-					else
-						gioitinh="Nữ";
-					content += "Giới tính : " + gioitinh + "<br>";
-					content += "Vào lúc : " + lichhenservice.doichuoitungay(lichHen.getThoiGian()) + "<br>";
-					if (nhanVien != null) {
-						content += "Với bác sỹ : " + nhanVien.getTen() + "<br>";
-					}
-					content += "Vui lòng có mặt tại phòng khám để nhận được dịch vụ tốt nhất!" + "<br>";
-					content += "<br>";
-					content += "Cảm ơn bạn đã đặt lịch ở phòng khám chúng tôi.";
-					messageHelper.setText(content, true);
-					javaMailSender.send(message);
-					}catch (Exception e) {
+
+						messageHelper.setSubject("Xác nhận đặt lịch tại phòng khám");
+						messageHelper.setFrom("phongkhamlv@gmail.com", "Phòng khám LV");
+						messageHelper.setTo(benhNhan.getEmail());
+						String content = "<b>Xin chào " + benhNhan.getTen() + "</b> <br>";
+						content += "Chúng tôi đã nhận được lịch hẹn trước của bạn tại phòng khám với các thông tin như sau : <br>";
+						content += "Họ và tên :" + benhNhan.getTen() + "<br>";
+						content += "Ngày sinh : " + benhnhanservice.doichuoitungay(benhNhan.getNgaySinh()) + "<br>";
+						String gioitinh = "";
+						if (benhNhan.isGioiTinh())
+							gioitinh = "Nam";
+						else
+							gioitinh = "Nữ";
+						content += "Giới tính : " + gioitinh + "<br>";
+						content += "Vào lúc : " + lichhenservice.doichuoitungay(lichHen.getThoiGian()) + "<br>";
+						if (nhanVien != null) {
+							content += "Với bác sỹ : " + nhanVien.getTen() + "<br>";
+						}
+						content += "Vui lòng có mặt tại phòng khám để nhận được dịch vụ tốt nhất!" + "<br>";
+						content += "<br>";
+						content += "Cảm ơn bạn đã đặt lịch ở phòng khám chúng tôi.";
+						messageHelper.setText(content, true);
+						javaMailSender.send(message);
+					} catch (Exception e) {
 					}
 				});
 				thread.start();
@@ -285,15 +329,34 @@ public class MyController {
 				redirectAttributes.addFlashAttribute("message", "Bạn đã đặt lịch hẹn thành công");
 				return "redirect:/thong-bao";
 			}
-		}
-		else
-		{
+		} else {
 			redirectAttributes.addFlashAttribute("message", "Mỗi ngày bạn chỉ đặt được 1 lịch hẹn ");
 			return "redirect:/thong-bao";
 		}
-		
+
 		redirectAttributes.addFlashAttribute("message", " Bạn đã đặt lịch hẹn thất bại");
 		return "redirect:/dat-lich";
 	}
 	
+	@PostMapping("/capnhat")
+	public String update(@ModelAttribute("benhNhan") BenhNhan benhNhan,Principal principal,
+			 RedirectAttributes redirectAttributes) throws IOException {
+			BenhNhan bn = new BenhNhan();
+			bn=benhnhanservice.GetOneBenhNhanByUser(principal.getName());
+		
+			benhNhan.setId(bn.getId());
+			benhNhan.setTaiKhoan(bn.getTaiKhoan());
+			benhNhan.setNgaySinh(bn.getNgaySinh());
+			int ketquaPUT = benhnhanservice.PUTBenhNhan(benhNhan);
+			if (ketquaPUT == 200) {
+				
+				redirectAttributes.addFlashAttribute("thanhcong", "Cập nhật thành công!");
+				return "redirect:/";
+			} else {
+				redirectAttributes.addFlashAttribute("thatbai", "Cập nhật thất bại!");
+				return "redirect:/thong-tin";
+
+			}
+	}
+
 }
--- KhoaLuanWeb/src/main/java/com/example/demo/service/LichHenService.java
+++ KhoaLuanWeb/src/main/java/com/example/demo/service/LichHenService.java
@@ -7,21 +7,25 @@ import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.stereotype.Repository;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-
 import enity.LichHen;
 import enity.Role;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonParser;
 
 @Repository
 public class LichHenService {
 	
 	static String POST_LICH_HEN="http://localhost:5001/lichhen/insert";
 	static String GET_LICH_HEN_TRONG_NGAY="http://localhost:5001/lichhen/ktralichhenbn";
+	static String GET_ALL_LICH_HEN_BY_BN="http://localhost:5001/lichhen/getlichhenbybn";
 	
 	
 	/**
@@ -107,5 +111,45 @@ public class LichHenService {
 		}
 
 		return lh;
+	}
+	
+	public  List<LichHen>  GetAllLichHenByBenhNhan(Long id) throws IOException {
+		List<LichHen>getall=new ArrayList<>();
+	    URL urlForGetRequest = new URL(GET_ALL_LICH_HEN_BY_BN+"/"+id);
+	    String readLine = null;
+	    HttpURLConnection conection = (HttpURLConnection) urlForGetRequest.openConnection();
+	    conection.setRequestMethod("GET"); // set userId its a sample here
+	    conection.setRequestProperty("Content-Type", "application/json");
+	    int responseCode = conection.getResponseCode();
+
+	    if (responseCode == HttpURLConnection.HTTP_OK) {
+	        BufferedReader in = new BufferedReader(
+	            new InputStreamReader(conection.getInputStream()));
+	        String response = new String();
+	        while ((readLine = in .readLine()) != null) {
+	            response+=(readLine);
+	        } in .close();
+	        if(responseCode==200)
+	        {
+	        	Gson gson = new GsonBuilder()
+	        		    .setDateFormat("yyyy-MM-dd")
+	        		    .create();
+		        JsonParser parser = new JsonParser();
+		        JsonArray object = (JsonArray) parser.parse(response);// response will be the json String
+		        LichHen[] lichhenList = gson.fromJson(object, LichHen[].class);
+		        	
+		        for(int i=0;i<lichhenList.length;i++)
+		        	getall.add(lichhenList[i]);
+	        }
+	        else
+	        {
+	        	return null;
+	        }
+	        
+	    } else {
+	        System.out.println("GET NOT WORKED");
+	    }
+		return getall;
+
 	}
 }
